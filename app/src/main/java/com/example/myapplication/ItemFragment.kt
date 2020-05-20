4f2e536de2cab9274ae82fa5532296999fdb8171
ShinichiroFunatsu <sfunatsu.git@gmail.com>
2020-05-20 14:02:19 +0900

    extract animation class to another file

--- app/src/main/java/com/example/myapplication/ItemFragment.kt
+++ app/src/main/java/com/example/myapplication/ItemFragment.kt
@@ -1,24 +1,16 @@
 package com.example.myapplication
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.TimeInterpolator
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewPropertyAnimator
-import android.widget.FrameLayout
 import androidx.core.view.doOnNextLayout
-import androidx.core.view.marginLeft
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.animation.SlideInAnimation
 import com.example.myapplication.dummy.DummyContent
-import com.google.android.material.animation.AnimationUtils
 import kotlinx.android.synthetic.main.fragment_item_list.view.*
-import kotlin.properties.Delegates
 
 class ItemFragment : Fragment() {
 
@@ -83,74 +75,4 @@ class ItemFragment : Fragment() {
 
 }
 
-class SlideInAnimation() {
 
-    private var width by Delegates.notNull<Int>()
-    private var currentAnimator: ViewPropertyAnimator? = null
-    private lateinit var _child: View
-
-    fun initAnimationParam(width: Int, child: View) {
-        this.width = width
-        this._child = child
-    }
-
-    fun slideIn(onAnimationStart: () -> Unit) {
-        if (currentAnimator != null) {
-            currentAnimator!!.cancel()
-            _child.clearAnimation()
-        }
-        animateChildTo(
-            _child,
-            -width,
-            ENTER_ANIMATION_DURATION.toLong(),
-            AnimationUtils.LINEAR_OUT_SLOW_IN_INTERPOLATOR,
-            ENTER_START_DELAY.toLong(),
-            onAnimationStart = onAnimationStart
-        )
-    }
-
-    fun slideOut(onAnimationStart: () -> Unit) {
-        if (currentAnimator != null) {
-            currentAnimator!!.cancel()
-            _child.clearAnimation()
-        }
-        animateChildTo(
-            _child,
-            0,
-            EXIT_ANIMATION_DURATION.toLong(),
-            AnimationUtils.FAST_OUT_LINEAR_IN_INTERPOLATOR,
-            onAnimationStart = onAnimationStart
-        )
-    }
-    private fun animateChildTo(
-        child: View,
-        targetX: Int,
-        duration: Long,
-        interpolator: TimeInterpolator,
-        startDelay: Long = 0,
-        onAnimationStart: () -> Unit
-    ) {
-        currentAnimator = child
-            .animate()
-            .translationX(targetX.toFloat())
-            .setInterpolator(interpolator)
-            .setDuration(duration)
-            .setStartDelay(startDelay)
-            .setListener(
-                object : AnimatorListenerAdapter() {
-                    override fun onAnimationStart(animation: Animator?) {
-                        onAnimationStart()
-                    }
-                    override fun onAnimationEnd(animation: Animator) {
-                        currentAnimator = null
-                    }
-                })
-    }
-
-    companion object {
-        private const val ENTER_START_DELAY = 120
-        private const val ENTER_ANIMATION_DURATION = 260
-        private const val EXIT_ANIMATION_DURATION = 275
-    }
-
-}
---
+++ app/src/main/java/com/example/myapplication/animation/SlideInAnimation.kt
@@ -0,0 +1,83 @@
+package com.example.myapplication.animation
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.TimeInterpolator
+import android.view.View
+import android.view.ViewPropertyAnimator
+import com.google.android.material.animation.AnimationUtils
+import kotlin.properties.Delegates
+
+class SlideInAnimation() {
+
+    private var width by Delegates.notNull<Int>()
+    private var currentAnimator: ViewPropertyAnimator? = null
+    private lateinit var _child: View
+
+    fun initAnimationParam(width: Int, child: View) {
+        this.width = width
+        this._child = child
+    }
+
+    fun slideIn(onAnimationStart: () -> Unit) {
+        if (currentAnimator != null) {
+            currentAnimator!!.cancel()
+            _child.clearAnimation()
+        }
+        animateChildTo(
+            _child,
+            -width,
+            ENTER_ANIMATION_DURATION.toLong(),
+            AnimationUtils.LINEAR_OUT_SLOW_IN_INTERPOLATOR,
+            ENTER_START_DELAY.toLong(),
+            onAnimationStart = onAnimationStart
+        )
+    }
+
+    fun slideOut(onAnimationStart: () -> Unit) {
+        if (currentAnimator != null) {
+            currentAnimator!!.cancel()
+            _child.clearAnimation()
+        }
+        animateChildTo(
+            _child,
+            0,
+            EXIT_ANIMATION_DURATION.toLong(),
+            AnimationUtils.FAST_OUT_LINEAR_IN_INTERPOLATOR,
+            onAnimationStart = onAnimationStart
+        )
+    }
+
+    private fun animateChildTo(
+        child: View,
+        targetX: Int,
+        duration: Long,
+        interpolator: TimeInterpolator,
+        startDelay: Long = 0,
+        onAnimationStart: () -> Unit
+    ) {
+        currentAnimator = child
+            .animate()
+            .translationX(targetX.toFloat())
+            .setInterpolator(interpolator)
+            .setDuration(duration)
+            .setStartDelay(startDelay)
+            .setListener(
+                object : AnimatorListenerAdapter() {
+                    override fun onAnimationStart(animation: Animator?) {
+                        onAnimationStart()
+                    }
+
+                    override fun onAnimationEnd(animation: Animator) {
+                        currentAnimator = null
+                    }
+                })
+    }
+
+    companion object {
+        private const val ENTER_START_DELAY = 120
+        private const val ENTER_ANIMATION_DURATION = 260
+        private const val EXIT_ANIMATION_DURATION = 275
+    }
+
+}
