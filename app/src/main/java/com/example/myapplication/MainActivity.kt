17c804b5b4e828009c8bd3739ef4d71be36a7566
ShinichiroFunatsu <sfunatsu.git@gmail.com>
2020-05-20 17:16:05 +0900

    optimize import

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,8 @@
 package com.example.myapplication
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.TimeInterpolator
 import android.os.Bundle
-import android.view.ViewGroup
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.ViewPropertyAnimator
-import android.widget.LinearLayout
-import com.google.android.material.animation.AnimationUtils
-
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
 
